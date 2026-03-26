02b7c5fda48c80fcedefcaf174a4a9a086cae2c8
Empty <emptypageworking@gmail.com>
2026-03-26 14:15:18 +0700

    left navigation and log out

--- oasis/app/(main)/(routes)/documents/page.tsx
+++ oasis/app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,37 @@
+"use client"
+
+import Image from "next/image"
+import { useUser } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
+import { PlusCircle } from "lucide-react"
+
 const DocumentsPage = () => {
+    const { user } = useUser()
+
+
     return (
-        <div>
-            <p>hhhh</p>
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+                src="/image-dark.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image 
+                src="/image-white.png"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="hidden dark:block"
+            />
+            <h2 className="text-lg font-medium ">
+                Welcome to {user?.firstName}&apos;s Oasis
+            </h2>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a note Oasis
+            </Button>
         </div>
     )
 }
