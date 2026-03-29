72287bbe2ffab7d2002a807f0ff6d3cf36d6972c
Empty <emptypageworking@gmail.com>
2026-03-29 09:26:40 +0700

    correction of the connection

--- oasis/convex/documents.ts
+++ oasis/convex/documents.ts
@@ -1,85 +1,69 @@
 import { v } from "convex/values";
-import { mutation, query } from './_generated/server'
+import { mutation, query } from './_generated/server';
 import { Doc, Id } from "./_generated/dataModel";
 
 export const archive = mutation({
     args: { id: v.id("documents") },
     handler: async (ctx, args) => {
-
-        const identity = await ctx.auth.getUserIdentity()
+        const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-            throw new Error("Not authenticate")
+            throw new Error("Not authenticated");
         }
 
-        const userId = identity.subject
+        const userId = identity.subject;
 
-        const existingDocument = await ctx.db.get(args.id)
+        const existingDocument = await ctx.db.get(args.id);
 
         if (!existingDocument) {
-            throw new Error("Not found")
+            throw new Error("Not found");
         }
 
         if (existingDocument.userId !== userId) {
-            throw new Error("Unauthorized")
+            throw new Error("Unauthorized");
         }
 
         const recursiveArchive = async (documentId: Id<"documents">) => {
             const children = await ctx.db
                 .query("documents")
-                .withIndex("by_user_parent", (q) => (
+                .withIndex("bt_user_parent", (q) => 
                     q
                         .eq("userId", userId)
                         .eq("parentDocument", documentId)
-                ))
-                .collect()
+                )
+                .collect();
 
             for (const child of children) {
                 await ctx.db.patch(child._id, {
                     isArchived: true,
-                })
+                });
 
-                await recursiveArchive(child._id)
+                await recursiveArchive(child._id);
             }
-
-        }
+        };
 
         const document = await ctx.db.patch(args.id, {
             isArchived: true,
-        })
+        });
 
-        recursiveArchive(args.id)
+        await recursiveArchive(args.id);
 
-        return document
+        return document;
     }
-})
-
-// export const get = query({
-//     handler: async (ctx) => {
-//         const identity = await ctx.auth.getUserIdentity()
-
-//         if (!identity) {
-//             throw new Error("Not authenticated")
-//         }
-
-//         const documents = await ctx.db.query("documents").collect()
-        
-//         return documents
-//     }
-// })
+});
 
 export const getSidebar = query({
     args: {
         parentDocument: v.optional(v.id("documents"))
     },
     handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity()
+        const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-            throw new Error("Not authenticate")
+            throw new Error("Not authenticated");
         }
 
-        const userId = identity.subject
+        const userId = identity.subject;
 
         const documents = await ctx.db
             .query("documents")
@@ -92,11 +76,11 @@ export const getSidebar = query({
                 q.eq(q.field("isArchived"), false)
             )
             .order("desc")
-            .collect()
+            .collect();
 
-        return documents
+        return documents;
     }
-})
+});
 
 export const create = mutation({
     args: {
@@ -104,13 +88,13 @@ export const create = mutation({
         parentDocument: v.optional(v.id('documents'))
     },
     handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity()
+        const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
-            throw new Error("Not authenticated")
+            throw new Error("Not authenticated");
         }
 
-        const userId = identity.subject
+        const userId = identity.subject;
 
         const document = await ctx.db.insert('documents', {
             title: args.title,
@@ -118,8 +102,8 @@ export const create = mutation({
             userId,
             isArchived: false,
             isPublished: false,
-        })
+        });
 
-        return document
+        return document;
     }
-})
+});
