21ba8630690b13bf00d2e091ae90ec693b06c153
Empty <emptypageworking@gmail.com>
2026-04-02 14:24:18 +0700

    fix: async auth in middleware

--- oasis/middleware.ts
+++ oasis/middleware.ts
@@ -2,9 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(['/']);
 
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
+  const authObject = await auth();
+
   if (!isPublicRoute(request)) {
-    auth().protect(); 
+    authObject.protect();
   }
 });
 
@@ -12,6 +14,5 @@ export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    
   ],
 };
