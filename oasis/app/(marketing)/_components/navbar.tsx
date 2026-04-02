9bc693313f5611a6d54e4065fae4cc0b757a03ff
Empty <emptypageworking@gmail.com>
2026-04-02 14:37:52 +0700

    fix: final final middleware fix

--- oasis/app/(marketing)/_components/navbar.tsx
+++ oasis/app/(marketing)/_components/navbar.tsx
@@ -4,10 +4,8 @@ import { useScrollTop } from "@/hooks/use-scroll-top"
 import { cn } from "@/lib/utils"
 import { Logo } from "./logo"
 import { ModeToggle } from "@/components/mode-toggle"
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { useAuth } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
-import { SignInButton, UserButton } from "@clerk/clerk-react"
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button"
 import { Spinner } from "@/components/spinner"
 import Link from "next/link"
